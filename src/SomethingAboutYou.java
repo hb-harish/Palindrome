78c0fa7f383604f74bffb373e28daa8f3c0cb323
harish <hb.harishbalaji@gmail.com>
2016-09-02 16:37:49 -0400

    debug output

--- src/SomethingAboutYou.java
+++ src/SomethingAboutYou.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
-public class SomethingAboutYou { public static void main( String[] args ) { 
+public class SomethingAboutYou { 
+	
+	public static void main( String[] args ) { 
 	
 	Scanner sc = new Scanner(System.in);
 
